0511303e68bac5d11f72fc0d5afe519493a71a0f
ei1333 <ei1333hobby@gmail.com>
2026-08-02 15:56:10 +0000

    fix

--- test/unittest/compositional-inverse-of-formal-power-series.test.cpp
+++ test/unittest/compositional-inverse-of-formal-power-series.test.cpp
@@ -41,18 +41,6 @@ Poly compositional_inverse_naive(Poly f, int deg) {
   return result;
 }
 
-Poly enumerate_coefficients_of_powers_naive(const Poly &f, const Poly &g,
-                                             int m) {
-  const int n = (int)f.size() - 1;
-  Poly result(m + 1), power(n + 1);
-  power[0] = mint(1);
-  for (int i = 0; i <= m; i++) {
-    result[i] = multiply_naive(power, g, n + 1)[n];
-    power = multiply_naive(power, f, n + 1);
-  }
-  return result;
-}
-
 int main() {
   Poly linear{0, 7};
   assert(compositional_inverse_of_formal_power_series<mint>(linear, 0) ==
@@ -94,26 +82,6 @@ int main() {
         Poly x(deg);
         x[1] = mint(1);
         assert(composed == x);
-      }
-    }
-  }
-
-  for (int n = 0; n <= 20; n++) {
-    for (int rep = 0; rep < 20; rep++) {
-      for (int nonzero_constant = 0; nonzero_constant < 2;
-           nonzero_constant++) {
-        Poly f(n + 1), g(n + 1);
-        if (nonzero_constant) {
-          do {
-            f[0] = mint((int64_t)rng());
-          } while (f[0] == mint(0));
-        }
-        for (int i = 1; i <= n; i++) f[i] = mint((int64_t)rng());
-        for (auto &value : g) value = mint((int64_t)rng());
-
-        const int m = rng() % (2 * n + 6);
-        assert(enumerate_coefficients_of_powers<mint>(f, g, m) ==
-               enumerate_coefficients_of_powers_naive(f, g, m));
       }
     }
   }
