5064a7ccad78b7623ee92de35bc40bb0bc251db3
ei1333 <ei1333hobby@gmail.com>
2026-08-10 14:52:36 +0000

    Migrate AOJ verification tests batch three

--- test/verify/aoj-2270.test.cpp
+++ test/verify/aoj-2270.test.cpp
@@ -1,41 +1,40 @@
-// competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2270
+// competitive-verifier: PROBLEM
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2270
 
-#include "../../template/template.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
 
 #include "../../other/mo-tree.hpp"
 
 #include "../../structure/others/binary-indexed-tree.hpp"
 
+using namespace std;
+
 int main() {
   int N, Q;
   cin >> N >> Q;
-  vector< int > A(N);
-  for(auto &a: A) cin >> a;
+  vector<int> A(N);
+  for (auto& a : A) cin >> a;
   auto vs = A;
   sort(begin(vs), end(vs));
   vs.erase(unique(begin(vs), end(vs)), end(vs));
-  for(auto &a: A) a = lower_bound(begin(vs), end(vs), a) - begin(vs);
+  for (auto& a : A) a = lower_bound(begin(vs), end(vs), a) - begin(vs);
   MoTree<> g(N);
   g.read(N - 1);
-  vector< int > K(Q);
-  for(int i = 0; i < Q; i++) {
+  vector<int> K(Q);
+  for (int i = 0; i < Q; i++) {
     int a, b;
     cin >> a >> b >> K[i];
     --a, --b;
     g.add(a, b);
   }
-  vector< int64 > ans(Q);
-  BinaryIndexedTree< int > bit(N);
-  int64 sum = 0;
-  auto add = [&](int i) {
-    bit.apply(A[i], 1);
-  };
-  auto erase = [&](int i) {
-    bit.apply(A[i], -1);
-  };
-  auto out = [&](int q) {
-    ans[q] = vs[bit.lower_bound(K[q])];
-  };
+  vector<int64_t> ans(Q);
+  BinaryIndexedTree<int> bit(N);
+  auto add = [&](int i) { bit.apply(A[i], 1); };
+  auto erase = [&](int i) { bit.apply(A[i], -1); };
+  auto out = [&](int q) { ans[q] = vs[bit.lower_bound(K[q])]; };
   g.build(add, erase, out);
-  for(auto &p: ans) cout << p << "\n";
+  for (auto& p : ans) cout << p << "\n";
 }
--- test/verify/aoj-2306.test.cpp
+++ test/verify/aoj-2306.test.cpp
@@ -1,16 +1,20 @@
-// competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2306
+// competitive-verifier: PROBLEM
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2306
 
-#include "../../template/template.hpp"
+#include <algorithm>
+#include <iostream>
 
 #include "../../math/matrix/square-matrix.hpp"
 #include "../../graph/others/enumerate-cliques.hpp"
 
+using namespace std;
+
 int main() {
   int N, M;
   cin >> N >> M;
-  SquareMatrix< bool, 100 > g;
-  SquareMatrix< int, 100 > h;
-  for(int i = 0; i < M; i++) {
+  SquareMatrix<bool, 100> g;
+  SquareMatrix<int, 100> h;
+  for (int i = 0; i < M; i++) {
     int x, y, z;
     cin >> x >> y >> z;
     --x, --y;
@@ -21,13 +25,13 @@ int main() {
   }
   auto cliques = enumerate_cliques(g);
   int ret = 0;
-  for(auto &clique : cliques) {
-    if(clique.size() == 1) continue;
+  for (auto& clique : cliques) {
+    if (clique.size() == 1) continue;
     int add = 0;
-    for(auto &i : clique) {
+    for (auto& i : clique) {
       int uku = 1 << 30;
-      for(auto &j : clique) {
-        if(i != j) uku = min(uku, h[i][j]);
+      for (auto& j : clique) {
+        if (i != j) uku = min(uku, h[i][j]);
       }
       add += uku;
     }
@@ -35,4 +39,3 @@ int main() {
   }
   cout << ret << "\n";
 }
-
