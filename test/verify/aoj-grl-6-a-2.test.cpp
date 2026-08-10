dffcab4814663b5fcece42af24ab8cc073c9ca9c
ei1333 <ei1333hobby@gmail.com>
2026-08-10 10:53:15 +0000

    Migrate AOJ flow verification tests

--- test/verify/aoj-grl-6-a-2.test.cpp
+++ test/verify/aoj-grl-6-a-2.test.cpp
@@ -1,14 +1,18 @@
+// clang-format off
 // competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A
+// clang-format on
 
-#include "../../template/template.hpp"
+#include <cstdio>
 
 #include "../../graph/flow/ford-fulkerson.hpp"
+
+using namespace std;
 
 int main() {
   int V, E;
   scanf("%d %d", &V, &E);
-  FordFulkerson< int > g(V);
-  for(int i = 0; i < E; i++) {
+  FordFulkerson<int> g(V);
+  for (int i = 0; i < E; i++) {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     g.add_edge(a, b, c);
--- test/verify/aoj-grl-6-b.test.cpp
+++ test/verify/aoj-grl-6-b.test.cpp
@@ -1,14 +1,18 @@
+// clang-format off
 // competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_B
+// clang-format on
 
-#include "../../template/template.hpp"
+#include <cstdio>
 
 #include "../../graph/flow/primal-dual.hpp"
+
+using namespace std;
 
 int main() {
   int V, E, F;
   scanf("%d %d %d", &V, &E, &F);
-  PrimalDual< int, int > g(V);
-  for(int i = 0; i < E; i++) {
+  PrimalDual<int, int> g(V);
+  for (int i = 0; i < E; i++) {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     g.add_edge(a, b, c, d);
