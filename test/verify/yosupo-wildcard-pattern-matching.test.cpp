8156f1f5923996fce106fdc33bafa08a7bdd1b76
ei1333 <ei1333hobby@gmail.com>
2026-08-10 14:28:01 +0000

    Migrate string verification tests

--- test/verify/yosupo-wildcard-pattern-matching.test.cpp
+++ test/verify/yosupo-wildcard-pattern-matching.test.cpp
@@ -1,15 +1,19 @@
+// clang-format off
 // competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/wildcard_pattern_matching
+// clang-format on
 
-#include "../../template/template.hpp"
-
-#include "../../string/wildcard-pattern-matching.hpp"
+#include <iostream>
+#include <string>
 
 #include "../../math/combinatorics/montgomery-mod-int.hpp"
+#include "../../string/wildcard-pattern-matching.hpp"
+
+using namespace std;
 
 int main() {
   string S, T;
   cin >> S >> T;
-  for(auto& a : wildcard_pattern_matching< modint998244353 >(S, T, '*')) {
+  for (auto& a : wildcard_pattern_matching<modint998244353>(S, T, '*')) {
     cout << a;
   }
   cout << endl;
