9b4b8947aeb60cc836dd89d00151c48a670db01c
ei1333 <ei1333hobby@gmail.com>
2026-08-10 14:46:35 +0000

    Migrate dynamic tree verification tests

--- test/verify/yosupo-dynamic-tree-vertex-set-path-composite-4.test.cpp
+++ test/verify/yosupo-dynamic-tree-vertex-set-path-composite-4.test.cpp
@@ -1,39 +1,44 @@
+// clang-format off
 // competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/dynamic_tree_vertex_set_path_composite
+// clang-format on
 
-#include "../../template/template.hpp"
+#include <iostream>
+#include <vector>
 
 #include "../../math/combinatorics/montgomery-mod-int.hpp"
-#include "../../structure/dynamic-tree/vertex-set-path-composite.hpp"
 #include "../../structure/dynamic-tree/dynamic-tree-builder-for-vertex.hpp"
+#include "../../structure/dynamic-tree/vertex-set-path-composite.hpp"
+
+using namespace std;
 
 using mint = modint998244353;
 
 int main() {
   int N, Q;
   cin >> N >> Q;
-  DynamicTreeBuilderForVertex< LinkCutTree, VertexSetPathComposite< mint > > g(N);
-  for(int i = 0; i < N; i++) {
+  DynamicTreeBuilderForVertex<LinkCutTree, VertexSetPathComposite<mint> > g(N);
+  for (int i = 0; i < N; i++) {
     mint a, b;
     cin >> a >> b;
     g.set_vertex(i, {a, b});
   }
-  for(int i = 1; i < N; i++) {
+  for (int i = 1; i < N; i++) {
     int a, b;
     cin >> a >> b;
     g.add_edge(a, b);
   }
   g.build();
-  while(Q--) {
+  while (Q--) {
     int t;
     cin >> t;
-    if(t == 0) {
+    if (t == 0) {
       int u, v, w, x;
       cin >> u >> v >> w >> x;
       g.evert(g.vs[u]);
       g.cut(g.vs[v]);
       g.evert(g.vs[w]);
       g.link(g.vs[w], g.vs[x]);
-    } else if(t == 1) {
+    } else if (t == 1) {
       int p;
       mint c, d;
       cin >> p >> c >> d;
