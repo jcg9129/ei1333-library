69724deefb346d75ec92f9ee115aa454e12324d3
ei1333 <ei1333hobby@gmail.com>
2026-08-10 10:41:56 +0000

    Migrate AOJ combinatorics verification tests

--- test/verify/aoj-dpl-5-j.test.cpp
+++ test/verify/aoj-dpl-5-j.test.cpp
@@ -1,13 +1,14 @@
+// clang-format off
 // competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_5_J
+// clang-format on
 
-#include "../../template/template.hpp"
+#include <iostream>
 
 #include "../../math/combinatorics/montgomery-mod-int.hpp"
-
 #include "../../math/combinatorics/partition-table.hpp"
 
 int main() {
   int N, K;
-  cin >> N >> K;
-  cout << partition_table< modint1000000007 >(N, K)[N][K] << endl;
+  std::cin >> N >> K;
+  std::cout << partition_table<modint1000000007>(N, K)[N][K] << std::endl;
 }
